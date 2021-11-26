faf175466e5672c165eeaf70e37b193013f8d0ef
gilym <92104855+gilym@users.noreply.github.com>
2021-11-26 14:33:47 +0900

    Update dashboard.php

--- responsi/dashboard.php
+++ responsi/dashboard.php
@@ -89,7 +89,13 @@ else{
 
     </div>
 
-    
+     <footer class="bg-light text-center text-lg-start" style="margin-top: 25%; " style="position: fixed;">
+  
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright: Inventaris 
+  </div>
+ 
+</footer>
     
 
 </body>
